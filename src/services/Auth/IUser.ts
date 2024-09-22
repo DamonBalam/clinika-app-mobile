21dd8ebcd0bf7ebbe5245e130e64f8d0c363124c
Arturo Saldivar <damonbalam@gmail.com>
2024-09-22 17:25:10 -0600

    feat: Add Role, Auditable, Response, AuthDataService, and IUser files
    This commit adds the following files:
    - Role.ts: Defines the IRole interface for role management.
    - Auditable.ts: Defines the IAuditable interface for auditing purposes.
    - Response.ts: Defines the IResponse and IResponseAuth interfaces for handling API responses.
    - AuthDataService.ts: Implements the AuthDataService class for handling authentication-related API calls.
    - IUser.ts: Defines the IUser interface for representing user data.
    
    These changes are necessary to support upcoming features related to authentication and role management.

---
+++ src/services/Auth/IUser.ts
@@ -0,0 +1,53 @@
+import IAuditable from "../Auditable";
+import { IRole } from "../Role";
+
+export interface IUser extends IAuditable {
+  id?: number | null;
+  name?: string;
+  first_lastname?: string;
+  last_lastname?: string;
+  apellidos: string;
+  email: string;
+  telefono: string;
+  codigo_area: string;
+  fecha_nacimiento: string;
+  rol: string;
+  nutricionista: boolean;
+  email_verified_at: boolean;
+  consultorio_id: number | null;
+  roles: IRole[];
+}
+
+export class Usuario implements IUser {
+  id?: number | null;
+  name: string;
+  first_lastname: string;
+  last_lastname: string;
+  apellidos: string;
+  email: string;
+  telefono: string;
+  codigo_area: string;
+  fecha_nacimiento: string;
+  rol: string;
+  nutricionista: boolean;
+  email_verified_at: boolean;
+  consultorio_id: number | null;
+  roles: IRole[];
+
+  constructor(user: IUser) {
+    this.id = user.id || null;
+    this.name = user.name || "";
+    this.first_lastname = user.first_lastname || "";
+    this.last_lastname = user.last_lastname || "";
+    this.apellidos = user.apellidos || "";
+    this.email = user.email || "";
+    this.telefono = user.telefono || "";
+    this.codigo_area = user.codigo_area || "";
+    this.fecha_nacimiento = user.fecha_nacimiento || "";
+    this.rol = user.rol || "";
+    this.nutricionista = user.nutricionista || false;
+    this.email_verified_at = user.email_verified_at || false;
+    this.consultorio_id = user.consultorio_id || null;
+    this.roles = user.roles || "";
+  }
+}
---
+++ src/stores/auth.ts
@@ -0,0 +1,82 @@
+import { defineStore } from "pinia";
+import { authDataServices } from "src/services/Auth/AuthDataService";
+import { Cookies } from "quasar";
+import { IUser } from "src/services/Auth/IUser";
+
+export interface AuthState {
+  counter: number;
+  user: IUser;
+  token: string;
+  initLoader: boolean;
+}
+
+export const useAuthStore = defineStore("auth", {
+  state: () =>
+    ({
+      user: {},
+      token: "",
+      initLoader: false,
+    } as AuthState),
+
+  getters: {
+    isAuth: (state) => state.token !== "",
+    getUser: (state) => state.user,
+    getLoader: (state) => state.initLoader,
+  },
+  actions: {
+    setLoader(payload: boolean) {
+      this.initLoader = payload;
+    },
+    setUser(payload: any) {
+      this.user = payload.user;
+      this.token = payload.token;
+    },
+    setLocalStorage(payload: any) {
+      /* Cookies */
+      Cookies.set("user", payload.user);
+      Cookies.set("access_token", payload.token);
+    },
+    setLocalStorageWithTime(payload: any) {
+      /* Cookies */
+      Cookies.set("user", payload.user, {
+        expires: 100,
+      });
+      Cookies.set("access_token", payload.token, {
+        expires: 100,
+      });
+    },
+    login(payload: any) {
+      this.setUser(payload);
+      this.setLocalStorage(payload);
+      this.router.push("/inicio");
+    },
+    loginWithCookies(payload: any) {
+      this.setUser(payload);
+      this.setLocalStorageWithTime(payload);
+      this.router.push("/inicio");
+    },
+    async logout() {
+      try {
+        // await authDataServices.logout();
+        // this.deleteLocalStorage();
+
+        this.user = {};
+        this.token = "";
+        /* Cookies */
+        Cookies.remove("user");
+        Cookies.remove("access_token");
+        this.router.push("/login");
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    deleteLocalStorage() {
+      // this.user = {};
+      // this.token = '';
+      // /* Cookies */
+      // Cookies.remove('user');
+      // Cookies.remove('access_token');
+      // this.router.push('/login');
+    },
+  },
+});
